deb203bde459a8e1aa49bf65fd3906aa947593ae
suaib <suaib8211@gmail.com>
2023-12-08 10:14:06 +0530

    Final commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,16 +5,18 @@ import ytImg from "../assets/yt.png";
 const Projects = () => {
   return (
     <div id="projects" className="max-ww[1040px m-auto md:pl-20 p-4 py-16">
-      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        Projects
+      </h1>
       <p className=" text-center py-8">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
-        molestiae quod, quae laboriosam quasi quas reprehenderit doloremque
-        voluptatem blanditiis quis ipsam voluptas in ducimus perferendis
-        veritatis, reiciendis saepe et laudantium.
+        "Create a powerful web application using the MERNS stack (MongoDB,
+        Express.js, React.js, Node.js, and Socket.io). Enhance user experiences,
+        enable real-time collaboration, and ensure scalability with this
+        versatile technology combination."
       </p>
       <div className=" grid sm:grid-cols-2 gap-12 ">
-        <ProjectItem img={cryptoImg} title='Crypto Dashboard'/>
-        <ProjectItem img={ytImg} title='Get Yt Subscriber'/>
+        <ProjectItem img={cryptoImg} title="Crypto Dashboard" />
+        <ProjectItem img={ytImg} title="Get Yt Subscriber" />
       </div>
     </div>
   );
